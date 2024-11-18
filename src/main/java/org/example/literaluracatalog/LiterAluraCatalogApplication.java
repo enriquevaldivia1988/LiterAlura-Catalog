38526f80ad9e67ef61bda35ab8aa61c04e32c33d
Enrique Valdivia <enriquevaldivia@outlook.com>
2024-11-18 17:48:13 -0500

    Initialize new Spring Boot project
    Added initial files and configurations for the LiterAlura-Catalog project, including Maven wrapper, main application class, properties, and tests. Also included `.gitignore`, `.gitattributes`, and POM file for dependency management.

---
+++ src/main/java/org/example/literaluracatalog/LiterAluraCatalogApplication.java
@@ -0,0 +1,13 @@
+package org.example.literaluracatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiterAluraCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/literaluracatalog/LiterAluraCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.literaluracatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
